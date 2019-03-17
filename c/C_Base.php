6a5c95573631648afc885d289d8d8465b7f68512
Krilll <43990419+Krilll@users.noreply.github.com>
2019-03-18 02:19:08 +0300

    Add files via upload

--- c/C_Base.php
+++ c/C_Base.php
@@ -1,41 +1,1 @@
-<?php
-//
-// Базовый контроллер сайта.
-//
-include_once ('m/User.php');
-
-abstract class C_Base extends C_Controller
-{
-    protected $title;		// заголовок страницы
-    protected $content;		// содержание страницы
-	protected $library;
-    function __construct()
-    {
-    }
-
-    protected function before()
-    {
-        $this->title = 'Кружки ';
-        $this->content = '';
-		$this->library = '';
-    }
-
-    //
-    // Генерация базового шаблонаы
-    //
-    public function render()
-    {
-        $get_user = new User();
-        if (isset($_SESSION['user_id'])) {
-            $user_info = $get_user->getUser($_SESSION['user_id']);
-        } else {
-            $user_info['name'] = false;
-        }
-        $vars = array('title' => $this->title, 'content' => $this->content,
-            'user' => $user_info, 'date' => date("d.m.Y"), 'library' => $this->library);
-
-        //$vars = array('title' => $this->title, 'content' => $this->content);
-        $page = $this->Template('v/v_main.php', $vars);
-        echo $page;
-    }
-}
+<?php//// Базовый контроллер сайта.//include_once ('m/User.php');abstract class C_Base extends C_Controller{    protected $title;		// заголовок страницы    protected $content;		// содержание страницы	protected $library;    function __construct()    {    }    protected function before()    {        $this->title = 'Кружки ';        $this->content = '';		$this->library = '';    }    //    // Генерация базового шаблонаы    //    public function render()    {        $get_user = new User();        if (isset($_SESSION['user_id'])) {            $user_info = $get_user->getUser($_SESSION['user_id']);        } else {            $user_info['name'] = false;        }        $vars = array('title' => $this->title, 'content' => $this->content,            'user' => $user_info, 'date' => date("d.m.Y"), 'library' => $this->library);        //$vars = array('title' => $this->title, 'content' => $this->content);        $page = $this->Template('v/v_main.php', $vars);        echo $page;    }}
